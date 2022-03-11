f5a042e80a1fa410a6b07c2f8a085a81a188d87d
RealRichi3 <Molunorichie@gmail.com>
2022-03-11 12:26:35 +0100

    Added implement.py file

---
+++ implement.py
@@ -0,0 +1,14 @@
+"""
+Implementing y=|x|
+"""
+
+
+def eq(sec):
+    """
+        Function to return the value of y for every X given
+    """
+    return abs(sec)
+
+for x in range(-5, 5):      # Given range of inputs for X cord
+    print(" X:{} and Y:{}").format(x, eq(x))        # Print X and Y values
+
---
+++ week6-challenge1.py
@@ -0,0 +1,13 @@
+"""
+Given question:
+SEND + MORE = MONEY
+"""
+sendMore = []
+
+for letter in "SENDMORE":
+    sendMore.append(letter)
+
+for items in "SENDMORE":
+    for i in range(0,10):
+        sendMore[i-1] = i
+        for 
